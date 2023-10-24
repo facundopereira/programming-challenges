cc254abd1efe7c41f4a3b8c457948c831c11b159
Facundo Pereira <pereirafacundo66@gmail.com>
2023-10-24 20:29:28 -0300

    comentarios en los desafios

--- Challenges/FizzBuzz.js
+++ Challenges/FizzBuzz.js
@@ -1,4 +1,4 @@
-console.log("hola")
+//Desafio FizzBuzz, en este desafio lo que hace es imprimir FizzBuzz en los multiplos de 3 y 5, Fizz en los multiplos de 3 y Buzz en los multiplos de 5 y si no son multiplos de ninguno de esas 3 coindiciones imprime el valor del numero
 
 for(i=1; i<= 100; i++){
     if((i % 5=== 0) && (i % 3 === 0)){
@@ -13,6 +13,8 @@ for(i=1; i<= 100; i++){
     else{
         console.log(i)
     }
-}
+};
+
+/*APRENDIZAJE*/
 
 //En mi primero for me habia quedado mal ya que la condicion de si es multipo de 3 y 5 estaba al final por lo tanto esa condicion nunca se iba a cumplir porque se iba a cumplir las otras condiciones que es divisible entre 3 y 5 pero luego de estudiarlo y ver que nunca era divisible entre 3 y 5 me di cuenta de ese error, tambien me habia dado cuenta que el ultimo else if tenia una conidicion innecesaria que decia () ya que cualquiera que no se cumpla que sea divisible entre 3 y 5 iba a ser mostrado igual "else if(!(i % 5=== 0) && !(i % 3 === 0))" pero con poner else ya era suficiente 
--- Challenges/Multiplos-3-y-5.js
+++ Challenges/Multiplos-3-y-5.js
@@ -1,3 +1,5 @@
+//Desafio Multiplos de 3 y 5,  en este desafio lo que hace es sumar todos los valores que son multiplos de 3 y 5 que sean menores que 1000,  los suma todos en una variable y una vez recorrido todo el bucle se imprime el valor que es igual a 233168
+
 let suma = 0;
 for (i=1; i<1000 ; i++){
     if(i % 3 === 0 || i % 5 === 0){
@@ -6,3 +8,7 @@ for (i=1; i<1000 ; i++){
 }
 
 console.log(suma);
+
+/*APRENDIZAJE*/
+
+//En mi primera solucion cometi un error en el cual habia creado dos condiciones o sea un "if" y un "else if" que uno era para los multiplos de 3 y el otro para los multiplos de 5 pero no era necesario ya que podia trabajarlo con el operador logico "||", tambien tuve un error que imprimia en cada suma el valor "suma" en cada operacion realizada por lo tanto tenia cientos de impresiones,  aunque eso era para corroborar que se ejecutaran correctamente las sumas, al final lo puse fuera del bucle el "console.log(suma" para que me diera el resultado final.
